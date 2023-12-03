9bfb1919fb719685f626d4a660459fa51314cec9
Forrest Smith <forrestthewoods@gmail.com>
2023-12-03 15:59:13 -0800

    compiles and runs but is still touching /usr

--- sample_cpp_app/main.cpp
+++ sample_cpp_app/main.cpp
@@ -1,6 +1,8 @@
 #include <fmt/core.h>
+#include <stdio.h>
 
 int main() {
+    printf("hello from printf\n");
     fmt::print("Hello world from C++ 👋\n");
     fmt::print("goodbye cruel world from C++ ☠️\n");
     return 0;
