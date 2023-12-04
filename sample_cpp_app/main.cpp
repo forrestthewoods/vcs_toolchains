a89a0d3b94ea139430bb30e15b77dd32e9cf5ebf
Forrest Smith <forrestthewoods@gmail.com>
2023-12-03 18:11:57 -0800

    printf works

--- sample_cpp_app/main.cpp
+++ sample_cpp_app/main.cpp
@@ -1,9 +1,9 @@
-#include <fmt/core.h>
+//#include <fmt/core.h>
 #include <stdio.h>
 
 int main() {
     printf("hello from printf\n");
-    fmt::print("Hello world from C++ 👋\n");
-    fmt::print("goodbye cruel world from C++ ☠️\n");
+    //fmt::print("Hello world from C++ 👋\n");
+    //fmt::print("goodbye cruel world from C++ ☠️\n");
     return 0;
 }
