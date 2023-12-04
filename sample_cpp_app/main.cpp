ec8ea2272297cb00618d3bdd6f6f73c1e7fa5f58
Forrest Smith <forrestthewoods@gmail.com>
2023-12-03 18:37:11 -0800

    this is maybe working?

--- sample_cpp_app/main.cpp
+++ sample_cpp_app/main.cpp
@@ -1,9 +1,7 @@
-//#include <fmt/core.h>
-#include <stdio.h>
+#include <fmt/core.h>
 
 int main() {
-    printf("hello from printf\n");
-    //fmt::print("Hello world from C++ 👋\n");
-    //fmt::print("goodbye cruel world from C++ ☠️\n");
+    fmt::print("Hello world from C++ 👋\n");
+    fmt::print("goodbye cruel world from C++ ☠️\n");
     return 0;
 }
