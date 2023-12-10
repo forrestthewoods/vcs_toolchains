12ff4fc9bf3d91cad573f48f330de4a0a77ba593
Forrest Smith <forrestthewoods@gmail.com>
2023-12-10 00:46:03 -0800

    cleanup

--- build_and_run_linux.sh
+++ build_and_run_linux.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 # Everything is relative to script dir
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_RELDIR=$(dirname "$0")
+echo "Script dir is $SCRIPT_DIR"
+
+SCRIPT_ABSDIR=$(readlink -f $SCRIPT_RELDIR)
 
 # Nuke old directories
-rm -rf "$SCRIPT_DIR/build"
-rm -rf "$SCRIPT_DIR/bin"
+rm -rf "$SCRIPT_RELDIR/build"
+rm -rf "$SCRIPT_RELDIR/bin"
 
 # Create dirs
-mkdir "$SCRIPT_DIR/build"
+mkdir "$SCRIPT_RELDIR/build"
 
 # Nuke environment
 export PATH=""
 
 # Move to build dir
-cd "$SCRIPT_DIR/build"
+cd "$SCRIPT_ABSDIR/build"
 
 # build
-../toolchains/linux/cmake/bin/cmake -G Ninja -D CMAKE_MAKE_PROGRAM=~/code/vcs_toolchains/toolchains/linux/ninja/ninja ..
+../toolchains/linux/cmake/bin/cmake -G Ninja -D CMAKE_MAKE_PROGRAM=$SCRIPT_ABSDIR/toolchains/linux/ninja/ninja ..
 ../toolchains/linux/ninja/ninja -v
 
 # navigate back
