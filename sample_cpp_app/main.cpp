eb06550558abc7fbb56776c5fba374ee105f7050
Forrest Smith <forrestthewoods@gmail.com>
2023-12-09 16:52:17 -0800

    still segfaulting

--- sample_cpp_app/main.cpp
+++ sample_cpp_app/main.cpp
@@ -1,6 +1,17 @@
 #include <fmt/core.h>
 
-int main() {
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    int x = 5;
+    x += 3;
+
+    auto bytes = malloc(42);
+
+    //printf("%d", x);
+    std::vector<int> nums;
+    nums.push_back(x);
+
     fmt::print("Hello world from C++ 👋\n");
     fmt::print("goodbye cruel world from C++ ☠️\n");
     return 0;
